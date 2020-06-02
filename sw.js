ef6c8361e4ef63d4c8df32c2fd16f9c05c124761
marcos <marcos.moreu@protonmail.com>
2020-06-03 01:40:54 +0200

    temporal buttons with text instead of icons in last screen

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 
 // Set a name for the current cache. Note that when version is changed, the pwa only updates autmotically after reloading!
 //Note that for automatic update, at one change need to be made in the app.js file (or in other files...)
-var version = 'v8.1';
+var version = 'v8.2';
 console.log(version)
 
 // Default files to always cache
