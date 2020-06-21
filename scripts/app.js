3e1220f0265e3c615e4845ac105e2549ab4b0e72
marcos <marcos.moreu@protonmail.com>
2020-06-21 18:51:19 +0200

    mylayerbutton removed when draw created

--- scripts/app.js
+++ scripts/app.js
@@ -709,6 +709,10 @@ document.getElementById("deteleFeature").onclick = function(){
 
   document.getElementById("backDeteleFeature").style.display = "none";
   document.getElementById("deteleFeature").style.display = "none";
+
+  //to delete from geoJSON
+selectedFeature.setStyle({color:'#ffffff'})
+  //to delete from cartodb
 }
 
 
@@ -1055,81 +1059,11 @@ planet_Button.button.style.height = '50px';
 planet_Button.button.style.transitionDuration = '.3s';
 
 
-// var myLayerIsOn = true;
-// var myLayer_Button = L.easyButton({
-//     id: 'myLayer',
-//     class:'easyButton',
-//     position: 'topright',
-//     //background:'images/forest.png',
-//     states: [{
-//       icon: iconLAYERS,
-//       //  background:"images/forest.png",
-//         stateName: 'check-mark',
-//         onClick: function(btn,map) {
-//           deflated.removeFrom(map)
-//           localStorageLayer.addTo(map)
-// ///////////////////////////////////////////////////////////////////////////
-// console.log(myLayerIsOn)
-// console.log(isFirstTime)
-// console.log(finalLayer)
-// console.log(myLayerIsOn)
-//         if(myLayerIsOn == true && isFirstTime==false){
-//
-//           console.log('if1')
-//           myLayerIsOn = false;
-//           myLayer_Button.button.style.backgroundColor = 'grey';
-//           if(finalLayer != null){
-//             finalLayer.removeFrom(map)
-//
-//           }
-//
-//         //  localStorageLayer.removeFrom(map);
-//          if(myLayerIsOn == true){
-//            console.log('if2')
-//
-//           // localStorageLayer.removeFrom(map);
-//            myLayer_Button.button.style.backgroundColor = 'grey';
-//
-//           }
-//         }
-//         else if(myLayerIsOn == false && isFirstTime==false){
-//           deflated.addTo(map)
-//           localStorageLayer.removeFrom(map)
-//
-//           console.log('if3')
-//
-//           myLayerIsOn = true;
-//           myLayer_Button.button.style.backgroundColor = 'black';
-//          //finalLayer.addTo(map)
-//
-//          if(finalLayer != null){
-//            finalLayer.addTo(map)
-//          }
-//
-//           if(myLayerIsOn == true){
-//             console.log('if4')
-//
-//          localStorageLayer.addTo(map)          //  layer1.addTo(map);
-//          myLayer_Button.button.style.backgroundColor = 'black';
-//
-//
-//          }
-//        }else if(isFirstTime==true && localStorageLayer ==null){ //for first load when
-//          myLayer_Button.button.style.backgroundColor = 'red';
-//           console.log('if5')
-// }
-//
-//
-//           return myLayerIsOn;
-//         }
-//
-//     }]
-// }).addTo(map); //always on as there will always be features in the map, even when first load
 var myLayerIsOn = true;
 
 var whichLayerIsOn = 'deflated';
 var myLayer_Button = L.easyButton({
-    id: 'myLayer',
+    id: 'myLayerButton',
     class:'easyButton',
     position: 'topright',
     //background:'images/forest.png',
@@ -1140,6 +1074,7 @@ var myLayer_Button = L.easyButton({
         onClick: function(btn,map) {
          //  deflated.removeFrom(map)
          // whichLayerIsOn = 'deflated'
+
         if(whichLayerIsOn == 'deflated'){
           deflated.removeFrom(map)
           localStorageLayer.addTo(map)
@@ -2015,6 +1950,8 @@ var  finalLength2Decimals
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var typeOfFeature;
 map.on('draw:created', function (e) {
+  //  document.getElementById('myLayerButton').disable()
+        myLayer_Button.removeFrom(map)
         created = true;
         if(cartoLoaded == true){
           cartoGeometries.removeFrom(deflated)
@@ -2450,6 +2387,8 @@ var boxContent;
 
 
       document.getElementById('Cancel').onclick = function(e){
+        myLayer_Button.addTo(map)
+
         featureType = 'initial';
         alreadyMovedUp = false;
         audioRecorded = false;
@@ -2503,7 +2442,7 @@ var boxContent;
 
              tempLayer.clearLayers()
              if(cartoLoaded == true){
-               cartoGeometries.addt(deflated)
+               cartoGeometries.addTo(deflated)
              }
           //   return finalAreaHa & finalAreaAcres & finalLength
 
@@ -2683,6 +2622,7 @@ var diffTimes;
 
   document.getElementById('share-download').onclick = function(e) {
               //  featureType = 'initial';
+              myLayer_Button.addTo(map)
 
                 sameSession = true;
                 alreadyMovedUp = false;
