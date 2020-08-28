8b72fecdcc0135a4f6bb60f46373f307429fe51c
marcos <marcos.moreu@protonmail.com>
2020-08-28 23:32:57 +0200

    modified modal. limit number. info redirects

--- scripts/app.js
+++ scripts/app.js
@@ -175,28 +175,88 @@ var tilesDb = {
 };
 
 /// function to request pw on first load or if incorrect of second... load
+// var requestPw = function(){
+//       var pocPw = '1111' //! 🙈 if you are looking at this line, please keep in mind that this very basic security measure is only to prevent, during the testing period,
+//       // unintended submission of open land data by users with limited technological skills
+//
+//       setTimeout(function(){
+//         document.getElementById('modal').style.display='block';
+//         document.getElementById('enteredPw').click()
+//       },1000)
+//
+//       var checkPw = setInterval(function(){
+//         var pwPlaceholder = document.getElementById('enteredPw').value
+//
+//         if(pwPlaceholder.length == 4){
+//           document.getElementById('login').disabled = false
+//           document.getElementById('login').style.opacity='1';
+//         }
+//         if(pwPlaceholder.length < 4){
+//           document.getElementById('login').style.opacity='0.3';
+//          document.getElementById('login').disabled = true
+//         }
+//       },200)
+//
+//       document.getElementById('login').onclick = function(){
+//         var pwPlaceholder = document.getElementById('enteredPw').value
+//         if(pwPlaceholder == pocPw){
+//
+//           clearInterval(checkPw)
+//           document.getElementById('enteredPw').style.backgroundColor = '#39F70F'
+//           setTimeout(function(){
+//             document.getElementById('modal').style.display='none';
+//             document.getElementById('pwForm').style.display='none';
+//           },300)
+//           localStorage.setItem('pwCorrect', true);
+//         }else{
+//           document.getElementById('enteredPw').style.backgroundColor = 'red'
+//           setTimeout(function(){
+//             document.getElementById('enteredPw').style.backgroundColor = 'white'
+//           },300)
+//         }
+//       }
+//     }
+//////////////////////////login initial modal  //////////////
+
+document.getElementById('loginInfo').onclick = function(){
+  window.location.href = 'pages/tutorial.html';
+}
+document.getElementById('loginUnlock').onclick = function(e){
+  e.preventDefault()
+  document.getElementById('loginUnlock').style.backgroundColor = '#D5D6D5'
+  document.getElementById('enteredPw').style.display = 'initial';
+  document.getElementById('login').style.display = 'initial';
+
+}
+
+////////////////////    login  input    ///////////
 var requestPw = function(){
       var pocPw = '1111' //! 🙈 if you are looking at this line, please keep in mind that this very basic security measure is only to prevent, during the testing period,
       // unintended submission of open land data by users with limited technological skills
 
       setTimeout(function(){
         document.getElementById('modal').style.display='block';
+        // document.getElementById('enteredPw').click()
       },1000)
 
       var checkPw = setInterval(function(){
         var pwPlaceholder = document.getElementById('enteredPw').value
 
         if(pwPlaceholder.length == 4){
+          document.getElementById('login').style.borderColor= 'grey'
           document.getElementById('login').disabled = false
           document.getElementById('login').style.opacity='1';
         }
-        if(pwPlaceholder.length != 4){
+        if(pwPlaceholder.length < 4){
+          document.getElementById('login').style.borderColor= 'white'
           document.getElementById('login').style.opacity='0.3';
          document.getElementById('login').disabled = true
         }
       },200)
 
-      document.getElementById('login').onclick = function(){
+      document.getElementById('login').onclick = function(e){
+        e.preventDefault() // to avoid page reload on first load!
+
         var pwPlaceholder = document.getElementById('enteredPw').value
         if(pwPlaceholder == pocPw){
 
@@ -1430,7 +1490,10 @@ var circleLT250
 var circleLT250Added = false
 var circleGT250Added = false
 
+
+
 var refreshGPSbutton = setInterval(function() { ///////////////////////////////////////// function to keep searching for gps position
+//  if(localStorage.getItem('pwCorrect')){}
     try {
         navigator.geolocation.watchPosition(findBuffer);
     } catch (err) {
