5c6ff5998c2c826015f8904dfe085cce0a9f8631
marcos <marcos.moreu@protonmail.com>
2020-08-29 17:12:05 +0200

    keyword functionality deactivated

--- scripts/app.js
+++ scripts/app.js
@@ -2823,16 +2823,16 @@ if (isIOS == false) {
 
 document.getElementById('shareWorldButton').onclick = function(e) {
   //first, we define the variables that store the attributes
-  propertiesGeoJSON = data.features[0].properties
-  landUses = propertiesGeoJSON.landUses;
+  // propertiesGeoJSON = data.features[0].properties
+  // landUses = propertiesGeoJSON.landUses;
 
   //script to check if keyword is added in the attribute field
-  if(landUses[0] == ':' && landUses[1] == 'e' && landUses[2] == 'a' && landUses[3] == 'r' && landUses[4] == 't' && landUses[5] == 'h' ){
-
-    //to remove the keyword from the attribute field that is shown in the map
-    var landUsesEmojiWithKeyword = propertiesGeoJSON.landUsesEmoji;
-    var lengthLandUsesEmojiWithKeyword = landUsesEmojiWithKeyword.length
-    landUsesEmoji = landUsesEmojiWithKeyword.slice(2,lengthLandUsesEmojiWithKeyword);
+  // if(landUses[0] == ':' && landUses[1] == 'e' && landUses[2] == 'a' && landUses[3] == 'r' && landUses[4] == 't' && landUses[5] == 'h' ){
+    //
+    // //to remove the keyword from the attribute field that is shown in the map
+    // var landUsesEmojiWithKeyword = propertiesGeoJSON.landUsesEmoji;
+    // var lengthLandUsesEmojiWithKeyword = landUsesEmojiWithKeyword.length
+    // landUsesEmoji = landUsesEmojiWithKeyword.slice(2,lengthLandUsesEmojiWithKeyword);
 
     setTimeout(function() {
         document.getElementById('shareWorldButton').style.display = 'none';
@@ -2867,10 +2867,11 @@ document.getElementById('shareWorldButton').onclick = function(e) {
         document.getElementById("point").style.display = "initial";
 
         ////////////////////////////       CARTO - POST DATA      //////////////////////////////////////////
-
+        //first, we define the variables that store the attributes
+        propertiesGeoJSON = data.features[0].properties
         //to assign each attribute to a variable, which will be added as columns to the DB
-      //  landUses = propertiesGeoJSON.landUses;
-      //  landUsesEmoji = propertiesGeoJSON.landUsesEmoji;
+       landUses = propertiesGeoJSON.landUses;
+       landUsesEmoji = propertiesGeoJSON.landUsesEmoji;
         // include the firebase url (if audio has been recorded)
         if (finalUrlAudio != null) {
             var audioLinkText = ' 🔊 AUDIO';
@@ -2930,10 +2931,10 @@ document.getElementById('shareWorldButton').onclick = function(e) {
 
     featureSent = true;
 
-  }else{
-    alert("🔑 🛑 During the prototyping phase, a KEYWORD must be added in the textbox to publish the spatial data. Click 'Download' and in the main screen click the 'Quetionmark' button. Then click the 'Yellow' button to request the KEYWORD. Thanks");
-
-  }
+  // }else{
+  //   alert("🔑 🛑 During the prototyping phase, a KEYWORD must be added in the textbox to publish the spatial data. Click 'Download' and in the main screen click the 'Quetionmark' button. Then click the 'Yellow' button to request the KEYWORD. Thanks");
+  //
+  // }
   finalAreaHa2Decimals = null
   finalLength2Decimals = null
   //totalTimeSpent = 0
